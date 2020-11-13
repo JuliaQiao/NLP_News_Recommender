47d3736d8f2ef5d31d8cd4bec4a2317ce0aa60b7
JuliaQiao <67869278+JuliaQiao@users.noreply.github.com>
2020-11-13 10:52:06 -0500

    project finalized, slides completed

--- Streamlit.py
+++ Streamlit.py
@@ -4,6 +4,42 @@ import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 from Recommender import News_Outlet_Recommender
 
+#style rendering through HTML
+
+Body_html = """
+  <style>
+  h1{
+    color: #E6E6FA;
+  }
+  h2{
+    color: #f73448;
+
+  }
+  h3{
+    color: #E6E6FA;
+  }
+  h4{
+    color: #E6E6FA;
+  }
+  h5{
+    color: #E6E6FA;
+  }
+
+    body{
+    color: #E6E6FA;
+    background-color: #1c1c1c;
+}
+}
+
+
+
+}
+</style>
+"""
+st.markdown(Body_html, unsafe_allow_html=True)
+
+
+#app text rendering
 st.markdown(
 '''
 # Digital News Subscription Recommender
@@ -14,50 +50,58 @@ st.markdown('''### On a scale of 0% to 100%, how interested are you in reading a
 #''')
 
 st.markdown('''#### COVID-19''')
-covid = st.slider('ie. pandemic impact, COVID-19 spread, vaccine research, global lockdown.', 0, 100 , 50, format="%d percent")
+st.write('ie. pandemic impact, COVID-19 spread, vaccine research, global lockdown.')
+covid = st.slider('',0, 100 , 50, format="%d percent")
 st.write("You're ", covid, '%', 'interested in reading about COVID-19.')
 
 
 st.markdown('''#
 #### Staying At Home''')
-
-home = st.slider('ie. working from home, education, parenting.', 0, 100 , 50, format="%d percent")
-st.write("You're ", home, '%', 'interested in reading about Staying at Home.')
+st.write('ie. working from home, education, parenting.')
+home = st.slider(' ', 0, 100 , 50, format="%d percent")
+st.write("You're ", home, '%', 'interested in reading about staying at home.')
 
 st.markdown('''#
 #### U.S. Politics''')
-us_politics = st.slider('ie. Trump, Biden, elections, Supreme Court.', 0, 100 , 50, format="%d percent")
-st.write("You're ", us_politics, '%', 'interested in reading about U.S. Politics.')
+st.write('ie. Trump, Biden, elections, Supreme Court.')
+us_politics = st.slider('  ', 0, 100 , 50, format="%d percent")
+st.write("You're ", us_politics, '%', 'interested in reading about U.S. politics.')
 
 st.markdown('''#
 #### Global Politics''')
-global_politics = st.slider('ie. China, Brexit & European Union, Hong Kong protests.', 0, 100 , 50, format="%d percent")
-st.write("You're ", global_politics, '%', 'interested in reading about Global Politics.')
+st.write('ie. China, Brexit & European Union, Hong Kong protests.')
+global_politics = st.slider('   ', 0, 100 , 50, format="%d percent")
+st.write("You're ", global_politics, '%', 'interested in reading about global politics.')
 
 st.markdown('''#
 #### Global Economy''')
-economy = st.slider('ie. economic analysis, (un)employeement.', 0, 100 , 50, format="%d percent")
-st.write("You're ", economy, '%', 'interested in reading about Global Economy.')
+st.write('ie. economic analysis, (un)employeement.')
+economy = st.slider('    ', 0, 100 , 50, format="%d percent")
+st.write("You're ", economy, '%', 'interested in reading about global economy.')
 
 st.markdown('''#
 #### Social Issues''')
-social = st.slider('ie. police brutality, gender equality, climate change.', 0, 100 , 50, format="%d percent")
-st.write("You're ", social, '%', 'interested in reading about Social Issues.')
+st.write('ie. police brutality, gender equality, climate change.')
+social = st.slider('     ', 0, 100 , 50, format="%d percent")
+st.write("You're ", social, '%', 'interested in reading about social issues.')
 
 st.markdown('''#
 #### Business''')
-business = st.slider('ie. business news, stock market, technology.', 0, 100 , 50, format="%d percent")
-st.write("You're ", business, '%', 'interested in reading about Business.')
+st.write('ie. business news, stock market, technology.')
+business = st.slider('      ', 0, 100 , 50, format="%d percent")
+st.write("You're ", business, '%', 'interested in reading about business.')
 
 st.markdown('''#
 #### Personal Development''')
-personal = st.slider('ie. health & wellness, self help, personal finance.', 0, 100 , 50, format="%d percent")
-st.write("You're ", personal, '%', 'interested in reading about Personal Development.')
+st.write('ie. health & wellness, self help, personal finance.')
+personal = st.slider('       ', 0, 100 , 50, format="%d percent")
+st.write("You're ", personal, '%', 'interested in reading about personal development.')
 
 st.markdown('''#
-#### Cars''')
-cars = st.slider('',0, 100 , 50, format="%d percent")
-st.write("You're ", cars, '%', 'interested in reading about Cars.')
+#### Hobbies''')
+st.write('ie. cars.')
+cars = st.slider('        ',0, 100 , 50, format="%d percent")
+st.write("You're ", cars, '%', 'interested in reading about hobbies.')
 
 
 outlet_df = pd.read_csv('outlet_df.csv', index_col = 0 )
@@ -66,33 +110,20 @@ outlet_df = pd.read_csv('outlet_df.csv', index_col = 0 )
 recommendation = News_Outlet_Recommender(outlet_df, covid/50, home/50, us_politics/50, global_politics/50, economy/50, social/50, business/50, personal/50, cars/50)
 
 
-#st.markdown('''
-            #based off recommendation''')
-
-#st.write(recommendation)
-
 st.write('''
 #
 ### *Based off your interests, we recommend that you subscribe to:*
-#''', recommendation)
-
-#st.write(recommendation)
-
-
+##''', recommendation)
 
 
 st.markdown(
 '''
 #
 #
-More details on this recommender:
+More details:
 
 [Github](https://github.com/JuliaQiao/NLP_News_Recommender)
 
 [LinkedIn](https://www.linkedin.com/in/juliaqiao/)
 #'''
 )
-
-#st.write(
-#f'Predicted Sale Price of House: ${pred:.2f}'
-#
